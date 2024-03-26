c2aef699aa76b977016382554ce162807bc2e2fa
Boris Khegay <bk250104@gmail.com>
2024-03-26 19:21:48 +0500

    Initial Commit

---
+++ GitHub/ViewController.swift
@@ -0,0 +1,18 @@
+/*
+ репозиторий - проект
+ коммит - сохраненная версия приложений
+ пуш - отправка в гитхаб
+ */
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
